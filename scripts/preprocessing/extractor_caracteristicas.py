6c20f96162bbb4a19ed2527b45f59d6776eb8f06
margomeza16 <117955840+margomeza16@users.noreply.github.com>
2022-11-27 15:45:41 -0500

    Delete extractor_caracteristicas.py

--- scripts/preprocessing/extractor_caracteristicas.py
+++
@@ -1,5 +0,0 @@
-import tensorflow as tf
-def extraer_caracteristicas(data_train):
-	extractor = tf.keras.applications.ResNet50V2(weights='imagenet', include_top=False,
-                                            input_shape=(224, 224, 3))
-	return extractor
