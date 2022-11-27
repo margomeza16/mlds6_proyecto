b9a98dc98a88fc279008e8c295913c45969544ac
margomeza16 <117955840+margomeza16@users.noreply.github.com>
2022-11-26 21:12:59 -0500

    Update prepoc_img.py

--- scripts/preprocessing/prepoc_img.py
+++ scripts/preprocessing/prepoc_img.py
@@ -48,4 +48,4 @@ def preproceso(ruta_orig,batch_size):
   	#Se aplica data augmentation sobre el conjunto de imágenes de entrenamiento
   	train_gen = train_datagen.flow(X_train_prepro, Y_train, batch_size=batch_size)
 
-  	return train_gen,X_train_prep,X_val_prep,X_test_prep
+  	return train_gen,X_train_prep,X_val_prep,X_test_prep,Y_train,Y_test,Y_val
